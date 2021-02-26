a4e0825e89281078de701c04bb75a9b7a7ce3b9e
sahidulcse12 <m.sahidulislam.14@gmail.com>
2021-02-26 10:42:24 +0600

    api used

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [nayoks, setNayoks] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setNayoks(data))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {
+          nayoks.map(nayok => <Nayok name={nayok.name} key={nayok.id} address={nayok.address}></Nayok>)
+        }
+
+
+      </header>
+    </div>
+  );
+}
+
+function Nayok(props) {
+  console.log(props)
+  // const values = Object.values(props);
+
+  const styleDiv = {
+    border: '5px solid white',
+    borderRadius: '10px',
+    margin: '20px',
+    padding: '20px'
+
+  }
+  return (
+    <div style={styleDiv}>
+      <h2>Name : {props.name}</h2>
+      <h2>Address : {props.address.city}</h2>
+    </div>
+
+  )
+}
+
+export default App;
